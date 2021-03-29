2e60e14a5682343d6591a739f5b1a9806977e2e6
DESKTOP-UQSIAMQ\User <asfaysal.bracu@gmail.com>
2021-03-30 04:08:24 +0600

    Config Data Updated

--- src/components/Login/firebase.config.js
+++ src/components/Login/firebase.config.js
@@ -1,10 +1,9 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyBHhOX8rtp2vahVcATkhTD_7CwARw6arrk",
-    authDomain: "fire-auth2.firebaseapp.com",
-    databaseURL: "https://fire-auth2.firebaseio.com",
-    projectId: "fire-auth2",
-    storageBucket: "fire-auth2.appspot.com",
-    messagingSenderId: "587131087182",
-    appId: "1:587131087182:web:f7e53e9007fe7760d7561f"
-  };
-  export default firebaseConfig;
+  apiKey: "AIzaSyAlUaSDeJRurqS2kyPSi_txw1kAzF7I5vs",
+  authDomain: "ema-john-simple-1010.firebaseapp.com",
+  projectId: "ema-john-simple-1010",
+  storageBucket: "ema-john-simple-1010.appspot.com",
+  messagingSenderId: "1070735274134",
+  appId: "1:1070735274134:web:fb9b2b39afbff89fb8d2b7"
+};
+export default firebaseConfig;
